d9c0835ca9b6bf5307b738a1f91b94316f781003
Elijah Insua <tmpvar@gmail.com>
2014-01-26 22:44:53 -0700

    code coverage

--- modemanager.js
+++ modemanager.js
@@ -1,59 +1,76 @@
-function ModeManager() {
-  this._modes = {};
+function ModeManager(debug) {
+  this.modes = {};
   this._mode = null;
-  this._defaultMode = null;
+  this.defaultMode = null;
+  this.debug = !!debug;
 }
 
 ModeManager.prototype.mode = function(mode, options) {
   if (mode) {
-    if (this._modes[mode]) {
-      if (this._mode && typeof this._modes[this._mode].deactivate === 'function') {
-        this._modes[this._mode].deactivate(mode);
+    if (this.modes[mode]) {
+      if (this._mode && typeof this.modes[this._mode].deactivate === 'function') {
+        this.modes[this._mode].deactivate(mode);
       }
 
       var old = this._mode;
       this._mode = mode;
 
-      if (typeof this._modes[mode].activate === 'function') {
-        this._modes[mode].activate(old, options);
+      if (typeof this.modes[mode].activate === 'function') {
+        this.modes[mode].activate(old, options);
       }
 
-      console.log(old, '->', mode);
+      this.debug && console.log(old, '->', mode);
     } else {
-      console.log('mode not registered:', mode);
+      this.debug && console.log('mode not registered:', mode);
     }
   }
 
   return this._mode;
 };
 
-ModeManager.prototype.add = function(name, fn) {
-  this._modes[name] = fn;
+ModeManager.prototype.add = function(name, obj, isDefault) {
+  this.modes[name] = obj;
+  if (isDefault) {
+    this.defaultMode = name;
+  }
+  return this;
 }
 
 ModeManager.prototype.handle = function(type, event) {
-  event.modeManager = this;
-  if (this._modes[this._mode] && this._modes[this._mode].handle) {
-    if (!this._modes[this._mode].handle(type, event)) {
+  if (event) {
+    event.modeManager = this;
+  }
+  var mode = this._mode;
+  var defaultMode = this.defaultMode;
 
-      if (this._defaultMode && this._modes[this._defaultMode].handle) {
-        return this._modes[this._defaultMode].handle(type, event);
-      }
-    } else {
-      return true;
+  if (!this.modes[mode] ||
+      !this.modes[mode].handle ||
+      !this.modes[mode].handle(type, event)
+     )
+  {
+    if (defaultMode &&
+        this.modes[defaultMode] &&
+        this.modes[defaultMode].handle
+       )
+    {
+      return this.modes[this.defaultMode].handle(type, event);
     }
+  } else {
+    return true;
   }
 }
 
 ModeManager.prototype.update = function(delta) {
 
-  if (this._modes[this._mode] && typeof this._modes[this._mode].update === 'function') {
-    this._modes[this._mode].update(delta);
+  if (this.modes[this._mode] && typeof this.modes[this._mode].update === 'function') {
+    this.modes[this._mode].update(delta);
   }
 
-  if (this._modes[this._defaultMode] && typeof this._modes[this._defaultMode].update === 'function') {
-    this._modes[this._defaultMode].update(delta);
+  if (this.modes[this.defaultMode] && typeof this.modes[this.defaultMode].update === 'function') {
+    this.modes[this.defaultMode].update(delta);
   }
+
+  return this;
 };
 
 if (typeof module !== 'undefined' && module.exports) {
--- test/test.js
+++ test/test.js
@@ -1,16 +1,188 @@
-var modemanager;
+var ModeManager;
 if (typeof require !== "undefined") {
-  modemanager = require("../"modemanager.js);
+  ModeManager = require("../modemanager.js");
 } else {
-  modemanager = window.modemanager;
+  ModeManager = window.ModeManager;
 }
 
 var ok = function(a, msg) { if (!a) throw new Error(msg || "not ok"); };
 
 describe('modemanager', function() {
-  describe('#', function() {
-    it('', function() {
+  describe('#add', function() {
+    it('should add mode objects by name', function() {
+      var m = new ModeManager();
+      var obj = {};
+      m.add('test', obj);
+      ok(obj === m.modes.test);
+    });
+
+    it('should setup the default mode if isDefault is passed', function() {
+      var m = new ModeManager();
+      var obj = {};
+      m.add('test', obj, true);
+      ok(m.defaultMode === 'test');
+    });
+
+    it('should not setup the default mode if isDefault is passed', function() {
+      var m = new ModeManager();
+      var obj = {};
+      m.add('test', obj);
+      ok(m.defaultMode === null);
+    });
+
+    it('should chain', function() {
+      var m = new ModeManager();
+      ok(m.add('a', {}) === m);
+    });
+  });
+
+  describe('#handle', function() {
+    it('should call the current mode handler if available', function(t) {
+      var m =(new ModeManager).add('test', {
+        handle: function(type, event) {
+          ok(type === 'blah');
+          ok(event.abc);
+
+          ok(event.modeManager === m);
+          t();
+        }
+      });
+
+      m.mode('test');
+      m.handle('blah', { abc : true });
+    });
+
+    it('should call the default mode if the current mode does not handle it', function(t) {
+      var m =(new ModeManager()).add('test', { handle : function() {} }).add('default', {
+        handle: function(type, event) {
+          ok(type === 'event type');
+
+          t();
+        }
+      }, true);
+
+      m.mode('test');
+      m.handle('event type', {});
+    });
+
+    it('should call the default mode if the current mode does not handle it (invalid mode)', function(t) {
+      var m =(new ModeManager()).add('test').add('default', {
+        handle: function(type, event) {
+          ok(type === 'event type');
+
+          t();
+        }
+      }, true);
+
+      m.mode('test');
+      m.handle('event type', {});
+    });
+
+    it('should return true when the current mode returns true', function() {
+      var m =(new ModeManager()).add('test', {
+        handle: function(type, event) {
+          return type;
+        }
+      }, true);
+
+      m.mode('test');
+
+      ok(m.handle(true, {}));
+      ok(!m.handle(false, {}));
+    });
+
+    it('should return true when the default mode returns true', function() {
+      var m =(new ModeManager()).add('test', {
+        handle: function(type, event) {
+          return type;
+        }
+      }, true);
+
+      ok(m.handle(true, {}));
+      ok(!m.handle(false, {}));
+    });
+
+  });
+
+  describe('#mode', function() {
+    it('should return the current mode if one is set', function() {
+      var m =(new ModeManager()).add('test', 'hi');
+      ok(m.mode('test') === 'test');
+      ok(m.mode() === 'test');
+    });
+
+    it('should call mode.activate if available', function() {
+      var out = '';
+      var m =(new ModeManager()).add('test', {
+        activate: function() {
+          out += 'a';
+        },
+        deactivate : function() {
+          out += 'd';
+        }
+      }).add('abc', true);
+
+      ok(m.mode('test'));
+      ok(m.mode('abc'));
+      ok(out === 'ad');
+    });
+  });
+
+  describe('#update', function() {
+    it('should chain', function() {
+      var m = new ModeManager();
+      ok(m === m.update())
+    });
+
+    it('should call the current mode.update if available', function(t) {
+      var m = new ModeManager();
+
+      m.add('test', {
+        update : function(d) {
+          ok(d === 1);
+          t();
+        }
+      })
       
+      m.mode('test');
+
+      m.update(1);
+    });
+
+    it('should call the default when the mode is not set', function(t) {
+      var m = new ModeManager();
+
+      m.add('test', {})
+
+      m.add('default', {
+        update : function(d) {
+          ok(d === 1);
+          t();
+        }
+      }, true)
+
+      m.mode('test');
+
+      m.update(1);
+    });
+  });
+
+  describe('composition', function() {
+    it('should stack as expected', function(t) {
+      var root = new ModeManager();
+      var m = new ModeManager();
+
+      root.add('m', m);
+
+      m.add('default', {
+        handle: function(type) {
+          ok(type === 'test');
+          t();
+        }
+      }, true);
+
+      root.mode('m');
+      root.handle('test');
     });
   });
 });
